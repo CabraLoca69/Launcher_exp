2b69240c53ee97445a2658f2468954875889ff44
CabraLoca69 <tobimanda.10@gmail.com>
2026-08-04 20:23:18 -0300

    quitando registry

--- helpers/data_manager.py
+++ helpers/data_manager.py
@@ -2,7 +2,6 @@ import os
 from data_access.datafiles import ICONS, db
 from helpers.safe_threading import safe_thread
 from helpers.file_manager import FileManager
-from platform_adapters.registry import CURRENT_OS, PLATFORM_METHODS
 from platform_adapters.platform_handler import PlatformHandler
 
 _favorites_limit= 6

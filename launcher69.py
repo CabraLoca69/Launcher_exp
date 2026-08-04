e3b21e9b17d0ba70e3f0596e48e259082014b04b
CabraLoca69 <tobimanda.10@gmail.com>
2026-08-04 20:21:17 -0300

    refactor instanciacion de GameLauncherController

--- launcher69.py
+++ launcher69.py
@@ -2,10 +2,9 @@ import sys
 from helpers.games_launcher import GameLauncherController
 
 def main():
-    launcher_controler = GameLauncherController()
     if "--launch" in sys.argv:
         game_name = sys.argv[sys.argv.index("--launch") + 1]
-        launcher_controler.launch_game(game_name)
+        GameLauncherController().launch_game(game_name)
                
     elif "--tk" in sys.argv:
         start_tk_ui()
@@ -16,12 +15,17 @@ def main():
 def start_tk_ui():
     from interface_files.tk_interface import TkLauncherUI
     init("Tk")
+    set_launcher_controller()
     TkLauncherUI().set()
 
 def start_qt_ui():
     from interface_files.qt_interface import QtLauncherUI
     init("Qt")
+    set_launcher_controller()
     QtLauncherUI.launch_ui()
+
+def set_launcher_controller():
+    GameLauncherController() 
 
 def init(interface: str):
     from interface_files.ui_handler import init_event_bus
